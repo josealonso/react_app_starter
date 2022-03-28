9aa4417d3e9145b0e046d01e5311bed743e71d16
josealonso <jose.alonso.programmer@gmail.com>
2022-03-28 20:52:16 +0200

    Added a form and a function to handle the "onSubmit" event

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ export default function App() {
       <h1>React Tutorial for Solidity developers</h1>
       <h2>Topics covered</h2> {/* component with single tag */}
       <TopicsList topics={topics} />
+      <AddTopic />
     </div>
   );
 }
@@ -23,5 +24,19 @@ function TopicsList({ topics }) {
         <li key={topic.id}>{topic.text}</li>
       ))}
     </ul>
+  )
+}
+
+function AddTopic() {
+  function handleAddTopic(event) {
+    event.preventDefault(); // This prevents a page refresh
+
+  }
+
+  return (
+    <form onSubmit={handleAddTopic}>
+      <input placeholder="Add topic" />
+      <button type="submit">Submit</button>
+    </form>
   )
 }
