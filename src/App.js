306ec3e274ed39ecd11433d0ff4bbabd020e13a1
josealonso <jose.alonso.programmer@gmail.com>
2022-03-28 20:09:54 +0200

    Created a React component and passed data with props, which are  custom attributes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import "./styles.css";
 
 export default function App() {
+  const topics = [
+    { id: 1, text: "React components", done: false },
+    { id: 2, text: "Rendering", done: false }
+  ];
+
   return (
     <div className="App">
       <h1>React Tutorial for Solidity developers</h1>
-      <h2>Topics covered</h2>
-      <ul>
-        <li>React components</li>
-        <li>Rendering</li>
-      </ul>
+      <h2>Topics covered</h2> {/* component with single tag */}
+      <TopicsList topics={topics} />
     </div>
   );
+}
+
+{/* Every component must return JSX elements */ }
+function TopicsList(props) {
+  console.log(props);
 }
